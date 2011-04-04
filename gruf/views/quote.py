b8b5a89ed72e4e31a598f345dbe8fb2aa9723bb5
Simon Maryasin <marsoft@ya.ru>
2011-04-04 11:52:41 +0400

    views/quote: возвращаем 404 при отсутствии цитаты

--- gruf/views/quote.py
+++ gruf/views/quote.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
-from flask import Module, render_template
-from gruf.database import db, Quote
+from flask import Module, render_template, abort
+from sqlalchemy.orm.exc import NoResultFound
+from gruf.database import Quote
 
 quote = Module(__name__)
 
 @quote.route('/<qid>/')
 def index(qid=None):
-    quote = Quote.query.filter_by(id=qid).one()
+    try:
+        quote = Quote.query.filter_by(id=qid).one()
+    except NoResultFound:
+        abort(404)
     return render_template('quote.html', quote=quote)
 
 @quote.route('/<qid>/rss.xml')
