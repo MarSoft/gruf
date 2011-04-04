ac1902df6023e26baf8279446604b7a4b5690a16
Simon Maryasin <marsoft@ya.ru>
2011-04-05 03:19:27 +0400

    views/qlist: wip

--- gruf/views/qlist.py
+++ gruf/views/qlist.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
-from flask import Module
+from flask import Module, render_template, abort
+from sqlalchemy.orm.exc import NoResultFound
+from gruf.database import Quote
 
 qlist = Module(__name__)
 
-@qlist.route('/')
-def index():
-    return 'Hello World!'
-
-@qlist.route('/sent-by/<nick>/')
-def sent_by(nick):
+# Получает отфильтрованный список цитат и правильно его отображает
+def display(quotes, mod):
     pass
 
-@qlist.route('/approved-by/<nick>/')
-def approved_by(nick):
-    pass
+@qlist.route('/<path:mod>')
+def index(mod):
+    return display(Quote.query.filter_by(
+        offensive=Quote.OFF_GOOD, state=Quote.STATE_APPROVED).all(),
+        mod)
+
+@qlist.route('/sent-by/<nick>/<path:mod>')
+def sent_by(nick, mod):
+    return display(Quote.query.filter_by(
+        sender=nick, state=Quote.STATE_APPROVED).all(), # добавить rejected?
+        mod)
+
+@qlist.route('/approved-by/<nick>/<path:mod')
+def approved_by(nick, mod):
+    return display(Quote.query.filter_by(
+        sender=nick, state=Quote.STATE_APPROVED).all(),
+        mod)
