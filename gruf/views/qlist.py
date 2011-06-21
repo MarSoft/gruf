c11e2c13986ef23d2f41d7743f76e37fc4773e21
Семён Марьясин <marsoft@ya.ru>
2011-06-21 23:02:55 +0400

    qlist: шаблон, заголовки, вызов без доп.параметра

--- gruf/views/qlist.py
+++ gruf/views/qlist.py
@@ -6,23 +6,34 @@ from gruf.database import Quote
 qlist = Module(__name__)
 
 # Получает отфильтрованный список цитат и правильно его отображает
-def display(quotes, mod):
-    pass
+def display(quotes, title, mod=None):
+    """
+    quotes: список объектов-цитат
+    title: заголовок страницы (т.е. описание того, что это за набор цитат)
+    mod: модификатор (rss.xml, subscribe...), отвечающий за способ вывода или действие
+    """
+    return render_template('qlist.html', **locals())
 
+@qlist.route('/')
 @qlist.route('/<path:mod>')
-def index(mod):
+def index(mod=None):
     return display(Quote.query.filter_by(
         offensive=Quote.OFF_GOOD, state=Quote.STATE_APPROVED).all(),
+        u'Quote list',
         mod)
 
+@qlist.route('/sent-by/<nick>')
 @qlist.route('/sent-by/<nick>/<path:mod>')
-def sent_by(nick, mod):
+def sent_by(nick, mod=None):
     return display(Quote.query.filter_by(
-        sender=nick, state=Quote.STATE_APPROVED).all(), # добавить rejected?
+        sender_id=nick, state=Quote.STATE_APPROVED).all(), # добавить rejected?
+        u'Цитаты, присланные пользователем %s' % nick,
         mod)
 
-@qlist.route('/approved-by/<nick>/<path:mod')
-def approved_by(nick, mod):
+@qlist.route('/approved-by/<nick>')
+@qlist.route('/approved-by/<nick>/<path:mod>')
+def approved_by(nick, mod=None):
     return display(Quote.query.filter_by(
-        sender=nick, state=Quote.STATE_APPROVED).all(),
+        sender_id=nick, state=Quote.STATE_APPROVED).all(),
+        u'Цитаты, одобренные пользователем %s' % nick,
         mod)
