ea29da1a2949a16cfebe8d7fc7e9ee8c8b0ee3d0
Семён Марьясин <marsoft@ya.ru>
2011-06-22 01:25:59 +0400

    login: хранение текущего пользователя
    Правда, входить ещё нельзя

--- gruf/views/login.py
+++ gruf/views/login.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 from flask import Module
+from flask import g, session
+from gruf.database import User
 
 login = Module(__name__)
+
+@login.before_request
+def lookup_current_user():
+    g.user = None
+    if 'openid' in session:
+        g.user = User.query.filter_by(openid=openid).first()
 
 @login.route('/')
 def index():
