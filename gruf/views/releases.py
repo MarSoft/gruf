a6d6854c4d2dbd41918bfa47b37ee0a401e06714
Семён Марьясин <marsoft@ya.ru>
2011-06-23 01:23:12 +0400

    releases: первое приближение (качать пока нельзя)

--- gruf/views/releases.py
+++ gruf/views/releases.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
-from flask import Module
+from flask import Module, render_template, abort, redirect
+from gruf.database import Release
 
 releases = Module(__name__)
 
 @releases.route('/')
 def index():
-    return 'Hello World!'
+    releases = Release.query.order_by(Release.version).all()
+    return render_template('releases.html', **locals())
+
+@releases.route('/create')
+def create():
+    abort(300)
+
+@releases.route('/<int:ver>/get/')
+@releases.route('/<int:ver>/get/<f>')
+def get(ver, f=None):
+    abourt(300)
+
+@releases.route('/<int:ver>/get-offensive/')
+@releases.route('/<int:ver>/get-offensive/<f>')
+def get_offensive(ver, f=None):
+    abort(300)
