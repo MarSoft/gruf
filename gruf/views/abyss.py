25075f7de44dc10633fab841018cd06c0315eec1
Семён Марьясин <marsoft@ya.ru>
2011-06-22 11:49:10 +0400

    Реализована бездна через qlist

--- gruf/views/abyss.py
+++ gruf/views/abyss.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import Module
+from gruf.views.qlist import display
+from gruf.database import Quote
 
 abyss = Module(__name__)
 
 @abyss.route('/')
-def index():
-    return 'Hello World!'
+@abyss.route('/<path:mod>')
+def index(mod=None):
+    return display(Quote.query.filter_by(
+        state=Quote.STATE_ABYSS).all(),
+        u'Бездна',
+        mod)
