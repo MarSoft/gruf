54b4b9022d857ed92dad9fded17355c82751bca9
Семён Марьясин <marsoft@ya.ru>
2011-06-22 15:58:45 +0400

    qlist: оффенсивные не через get, а по адресу

--- gruf/views/qlist.py
+++ gruf/views/qlist.py
@@ -6,32 +6,13 @@ from gruf.database import Quote
 qlist = Module(__name__)
 
 # Получает отфильтрованный список цитат и правильно его отображает
-def display(quotes, title, mod=None):
+def display(quotes, title, mod=None, offense=None):
     """
     quotes: запрос к таблице цитат
     title: заголовок страницы (т.е. описание того, что это за набор цитат)
     mod: модификатор (rss.xml, subscribe...), отвечающий за способ вывода или действие
+    offense: статус offensive (для вывода в шаблоне)
     """
-    if request.args.has_key('offensive') and request.args['offensive']:
-        offense = request.args['offensive']
-    else:
-        if g.user and g.user.showOffensiveByDefault:
-            offense = 'yes'
-        else:
-            offense = 'no' # незарегистрированным offensive не показываем
-
-    if offense in ['yes', '1', 1]:
-        offense = 'yes'
-        pass # ничего не фильтруем
-    elif offense in ['no', '0', 0]:
-        offense = 'no'
-        quotes = quotes.filter(not_(Quote.offensive == Quote.OFF_OFFENSIVE))
-    elif offense in ['only', '2', 2]:
-        offense = 'only'
-        quotes = quotes.filter_by(offensive=Quote.OFF_OFFENSIVE)
-    else:
-        abort(404, u'Неправильное значение переменной offensive=%s' % offense)
-
     if not mod: # по умолчанию - список в html с пагинацией
         # пагинация
         return render_template('list.html', **locals())
@@ -58,6 +39,29 @@ def index(mod=None):
         state=Quote.STATE_APPROVED),
         u'Все цитаты',
         mod)
+
+@qlist.route('/offensive/<offense>/')
+@qlist.route('/offensive/<offense>/<path:mod>')
+def offensivity(offense, mod=None):
+    quotes = Quote.query.filter_by(state=Quote.STATE_APPROVED)
+    if offense in ['yes', '1', 1]:
+        offense = 'yes'
+        title = u'Все цитаты (включая offensive)'
+        pass # ничего не фильтруем
+    elif offense in ['no', '0', 0]:
+        offense = 'no'
+        title = u'Все цитаты, кроме offensive'
+        quotes = quotes.filter(not_(Quote.offensive == Quote.OFF_OFFENSIVE))
+    elif offense in ['only', '2', 2]:
+        offense = 'only'
+        title = u'Только offensive-цитаты'
+        quotes = quotes.filter_by(offensive=Quote.OFF_OFFENSIVE)
+    else:
+        abort(404)
+
+    return display(quotes,
+        title,
+        mod, offense=offense)
 
 @qlist.route('/sent-by/<nick>')
 @qlist.route('/sent-by/<nick>/<path:mod>')
