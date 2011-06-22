c3df558a92a6346fb722b6fed006b6db85a5b54c
Семён Марьясин <marsoft@ya.ru>
2011-06-22 14:24:49 +0400

    main: вывод статистики

--- gruf/views/main.py
+++ gruf/views/main.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import Module, render_template
+from gruf.database import Quote, User, Comment, Release
 
 main = Module(__name__)
 
 @main.route('/')
 def index():
-    return render_template('main.html')
+    quotes = Quote.query
+    users = User.query
+    comments = Comment.query
+    releases = Release.query
+    return render_template('main.html', **locals())
