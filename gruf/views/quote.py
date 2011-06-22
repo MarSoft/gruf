8de6f6be39ee74e16388d9caa832ccba3d101629
Семён Марьясин <marsoft@ya.ru>
2011-06-22 19:37:31 +0400

    quote: edit: WIP

--- gruf/views/quote.py
+++ gruf/views/quote.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
-from flask import Module, render_template
+from flask import Module, g, request, render_template, abort
 from gruf.database import Quote
 
 quote = Module(__name__)
 
 @quote.route('/<int:qid>/')
-def index(qid=None):
+def index(qid):
     quote = Quote.query.get_or_404(qid)
     return render_template('quote.html', quote=quote)
 
 @quote.route('/<int:qid>/rss.xml')
-def rss(qid=None):
+def rss(qid):
     pass
 
-@quote.route('/<int:qid>/edit/')
-def edit(qid=None):
-    pass
+@quote.route('/<int:qid>/edit/', methods=['GET', 'POST'])
+def edit(qid):
+    if not g.user or not g.user.is_approver(): # только аппрувер может править цитаты
+        abort(403, u'У Вас недостаточно прав для выполнения этого действия')
+    quote = Quote.query.get_or_404(qid)
+    if quote.is_approved() and not g.user.is_admin(): # только админ может менять уже зааппрувленные цитаты
+        abort(403)
+
+    if request.method == 'POST':
+        pass
