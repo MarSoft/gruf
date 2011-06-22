1b285e4d4ef3a34592462583b2c3be0030f9f452
Семён Марьясин <marsoft@ya.ru>
2011-06-22 19:01:34 +0400

    quote: оптимизация
    Используем get_or_404(qid) вместо filter_by(id=qid)

--- gruf/views/quote.py
+++ gruf/views/quote.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import Module, render_template, abort
-from sqlalchemy.orm.exc import NoResultFound
+from flask import Module, render_template
 from gruf.database import Quote
 
 quote = Module(__name__)
 
 @quote.route('/<int:qid>/')
 def index(qid=None):
-    try:
-        quote = Quote.query.filter_by(id=qid).one()
-    except NoResultFound:
-        abort(404)
+    quote = Quote.query.get_or_404(qid)
     return render_template('quote.html', quote=quote)
 
 @quote.route('/<int:qid>/rss.xml')
